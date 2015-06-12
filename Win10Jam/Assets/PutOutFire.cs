5d5c7db9685959c42ba6efac4e02a5cfa9496e4d
Heather <h.macdougal@live.co.uk>
2015-06-12 13:01:37 +0100

    Steam and fire put out.

---
+++ Win10Jam/Assets/PutOutFire.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PutOutFire : MonoBehaviour
+{
+    public GameObject steam;
+
+    private int tapsToKill = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        tapsToKill--;
+
+        steam.GetComponent<ParticleSystem>().Play();
+
+        if (tapsToKill == 0)
+            Destroy(gameObject);
+    }
+}
